bac8df91b9100a8e9c018d38743a79a65d91b2dd
mainarchitech <mainarchitech@gmail.com>
2025-12-04 13:27:32 -0500

    Initial Commit

---
+++ LanStatusIndicator/LanStatusIndicatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  LanStatusIndicatorApp.swift
+//  LanStatusIndicator
+//
+//  Created by Константин  on 04.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct LanStatusIndicatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LanStatusIndicatorTests/LanStatusIndicatorTests.swift
@@ -0,0 +1,17 @@
+//
+//  LanStatusIndicatorTests.swift
+//  LanStatusIndicatorTests
+//
+//  Created by Константин  on 04.12.2025.
+//
+
+import Testing
+@testable import LanStatusIndicator
+
+struct LanStatusIndicatorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
